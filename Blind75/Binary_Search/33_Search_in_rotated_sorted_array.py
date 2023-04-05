d708823d7c1f289c564b21c0faedc7a0c1425580
Robin Felix <buildwithfelix@gmail.com>
2023-04-05 10:31:40 +0530

    testing a feature in github

--- Blind75/Binary_Search/33_Search_in_rotated_sorted_array.py
+++ Blind75/Binary_Search/33_Search_in_rotated_sorted_array.py
@@ -9,8 +9,8 @@ def search(nums, target):
             m = (l + r)// 2
             if nums[m] == target:
                 return m
-            if nums[m] > nums[r]:
-                l = m + 1
+            if nums[m] > nums[r] and nums[m] > target:
+                    l = m + 1
             else:
                 r = m - 1
         return -1
--- Dummy_codes/dummy.py
+++ Dummy_codes/dummy.py
@@ -1,20 +1,7 @@
 
+def add(a, b):
+    print()
 
-def dummy(numbers, target):
-    hashmap = {}
-    res = []
-    for i, n in enumerate(numbers):
-        diff = target - n
-        if diff in hashmap:
-            res.append(i)
-
-    return res
-
-
-numbers = [2,7,11,15]
-target = 9
-# Output: [1,2]
-print(dummy(numbers, target))
         
         
 
